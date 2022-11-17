d766e62125a4855d1058ffaa543c0e8f20b87669
Ductham08702 <102347246+Ductham08702@users.noreply.github.com>
2022-11-17 15:47:38 +0700

    firt commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+
+function App() {
+    return (
+        <div className="App">
+            Heelolo
+        </div>
+    );
+}
+
+export default App;
